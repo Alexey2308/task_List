a34cbda5d674fa4abc3d50097750202e99095a78
Alexey Elagin <alexyel@mail.ru>
2022-08-16 22:02:08 +0300

    extra Scanner removed

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,22 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-import static java.lang.System.in;
-
 
 public class Main {
 
     public static void main(String[] args) {
 
         List<String> list = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
         while (true) {
             System.out.println("Выберите одну из операций:" + "\n" + " 1-Добавить  2-Показать  3-Удалить 4-Найти");
-            Scanner scanner = new Scanner(in);
-            int numberOfChoice = scanner.nextInt();
+
+            int numberOfChoice = Integer.parseInt(scanner.nextLine());
 
             if (numberOfChoice == 1) {
-                Scanner s = new Scanner(in);
                 System.out.println("Какую покупку хотите добавить?");
-                String item = s.nextLine();
+                String item = scanner.nextLine();
                 list.add(item);
                 System.out.println("Итого в списке покупок:" + list.size());
                 continue;
@@ -35,24 +34,24 @@ public class Main {
                     System.out.println((i + 1) + "." + " " + list.get(i));
                 }
 
-                Scanner s1 = new Scanner(System.in);
                 System.out.println("Какую хотите удалить? Введите номер или название");
-                try {
-                    int itemDel = s1.nextInt();
+
+                if (scanner.hasNextInt()) {
+                    int itemDel = Integer.parseInt(scanner.nextLine());
                     String prodDel = list.get(itemDel - 1);
                     System.out.println("Покупка" + " " + prodDel + " " + "удалена");
                     list.remove(itemDel - 1);
-                } catch (Exception e) {
-                    String itemDel = s1.nextLine();
-                    System.out.println("Покупка" + " " + itemDel + " " + "удалена");
-                    list.remove(itemDel);
+                    continue;
                 }
+                String itemDel = scanner.nextLine();
+                System.out.println("Покупка" + " " + itemDel + " " + "удалена");
+                list.remove(itemDel);
             }
 
             if (numberOfChoice == 4) {
-                Scanner s2 = new Scanner(System.in);
+
                 System.out.println("Введите текст для поиска:");
-                String queryLower = s2.nextLine().toLowerCase();
+                String queryLower = scanner.nextLine().toLowerCase();
                 for (int i = 0; i < list.size(); i++) {
                     String itemLower = list.get(i).toLowerCase();
                     if (itemLower.contains(queryLower)) {
@@ -65,7 +64,5 @@ public class Main {
         }
 
     }
+
 }
-
-
-
